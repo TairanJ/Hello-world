4de433f43d6a21d412361488261d308b7a11bc49
TairanJ <54902728+TairanJ@users.noreply.github.com>
2019-11-06 10:34:16 -0500

    Updated from Brython Server: 11/6/2019 10:34:14 AM

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,7 @@
 """
 helloworld.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: <Tairan(Ryan) Ji>
+Credit: <None>
 
 Assignment:
 
@@ -9,4 +9,5 @@ Write and submit a Python program that prints the following:
 
 Hello, world!
 """
+print("Hello, world!")
 
